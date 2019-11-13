1b9b4b2ec157d198e3218f01f4d8df1388773495
Axel Chevarin <32080733+AxelChevarin@users.noreply.github.com>
2019-11-12 23:36:26 -0500

    Add file

---
+++ intranet/modele/sql/userManager.php
@@ -0,0 +1,25 @@
+<?php
+	function getListUser($db)
+	{
+		$users = array();
+		
+		$q = $db->query('SELECT id,droit,login,password,nom,prenom,age,sexe FROM user ORDER BY login');
+	
+		while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+		{
+			$users[] = $donnees;
+		}
+
+		return $users;
+	}
+
+	function getUserByLoginAndPwd($db, $login, $pwd)
+	{
+		$q = $db->prepare('SELECT * FROM user WHERE login = :login AND password = :password');
+		$q->execute(array('login' => $login,'password' => $pwd));
+	
+		$rslt = $q->fetch();
+
+		return $rslt;
+	}
+?>
